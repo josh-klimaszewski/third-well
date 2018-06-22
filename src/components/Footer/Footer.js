f147e86bc0c4a780075883fa6c68c75b3b2be916
Morgan McPeak <mcpeakmorgan@gmail.com>
2018-06-22 12:12:52 -0400

    media queries added

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -5,10 +5,10 @@ const Footer = () => {
     return (
         <div className="footer">
         <p class="footertext">Some sort of bold statement.</p>
-        <img src="assets/social_icons/twittericon.png" class="twitter"/>
-        <img src="assets/social_icons/fbicon.png" class="facebook"/>
-        <img src="assets/social_icons/linkedinicon.png" class="linkedin"/>
-        <img src="assets/social_icons/emailicon.png" class="email"/>
+        <a href="https://twitter.com/3rdwell" target="blank"><img src="assets/social_icons/twittericon.png" class="twitter"/></a>
+        <a href="https://m.facebook.com/3rdWell/" target="blank"><img src="assets/social_icons/fbicon.png" class="facebook"/></a>
+        <a href="https://www.linkedin.com/company/third-well-llc/" target="blank"><img src="assets/social_icons/linkedinicon.png" class="linkedin"/></a>
+        <a href="mailto:ben@thirdwell.com"><img src="assets/social_icons/emailicon.png" class="email"/></a>
         </div> 
        );
 }
